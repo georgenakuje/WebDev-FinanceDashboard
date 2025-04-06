45791b54a5dbe99594ac6b5d3e54d650c29d6895
meron_daniel <dmeron29@gmail.com>
2025-04-06 08:56:57 -0400

    confirm password fixed

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,7 +4,7 @@
       <input v-model="email" placeholder="Email" type="email" />
       <input v-model="username" placeholder="username" type="username" />
       <input v-model="password" placeholder="Password" type="password" />
-      <input v-model="password" placeholder="Confirm Password" type="password" />
+      <input v-model="confirmPassword" placeholder="Confirm Password" type="password" />
       <button @click="handleRegister">Register</button>
       <p class="signup-link">
         Already have an account?
@@ -18,15 +18,24 @@
     data() {
       return {
         email: '',
-        password: ''
+        username: '',
+        password: '',
+        confirmPassword: ''
       }
     },
     methods: {
       handleRegister() {
-        if (!this.email || !this.password) {
+        if (!this.email || !this.password || !this.confirmPassword) {
           alert('Fill in both fields.')
           return
         }
+
+        if (this.password != this.confirmPassword){
+          alert('Passwords do not match, try again!')
+          return
+        }
+
+
         // Save user (for demo: store in localStorage)
         const user = {
           email: this.email,
